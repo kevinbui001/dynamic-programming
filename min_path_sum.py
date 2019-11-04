71ae5c36df94c65aa51f733302257dac1faeb43f
Kevin Bui <kbui@Kevins-MacBook-Pro.local>
2019-11-03 22:48:06 -0800

    first commit

---
+++ min_path_sum.py
@@ -0,0 +1,53 @@
+import sys
+from typing import List
+import unittest
+
+
+class Solution:
+    def my_func(self, grid: List[List[int]]) -> int:
+        # dp[row][col] = min(dp[row-1][col], dp[row][col-1]) + grid[row][col]
+        # initalize dp
+        dp = [[sys.maxsize for j in range(len(grid[0]))]
+              for i in range(len(grid))]
+        # assing first dp (0,0) to grid (0,0)
+
+        dp[0][0] = grid[0][0]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # skip the first one
+                if i + j == 0:
+                    continue
+                else:
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+
+        return dp[-1][-1]
+
+
+s = Solution()
+
+m = [
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+]
+
+
+class MyTest(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+    def test_equal(self):
+        self.assertEqual(s.my_func(m), 7)
+
+
+def main():
+    print("---- RUNNING MAIN ----")
+    print(s.my_func(m))
+    print("---- DONE MAIN ----\n\n")
+
+
+# Main body
+if __name__ == '__main__':
+    main()
+    unittest.main()
